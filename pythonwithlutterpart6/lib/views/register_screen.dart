2c6983a50fb584c7e2eb393df8e03b82bc494597
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2021-11-03 12:30:21 +0530

    Fixed validation
    Signed-off-by: Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>

--- pythonwithlutterpart6/lib/views/register_screen.dart
+++ pythonwithlutterpart6/lib/views/register_screen.dart
@@ -5,8 +5,9 @@ import 'package:part5/service/http_Service.dart';
 import 'package:part5/styleguide/colors.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:part5/views/login_screen.dart';
+
 class RegisterScreen extends StatefulWidget {
-    final Function toggleView;
+  final Function toggleView;
 
   RegisterScreen({this.toggleView});
   @override
@@ -24,51 +25,49 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-        Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
         backgroundColor: Colors.blue[100],
         appBar: AppBar(
-           backgroundColor: Colors.blue[400],
-           elevation: 0.0,
+          backgroundColor: Colors.blue[400],
+          elevation: 0.0,
           title: Text('Register Screen'),
-              actions: <Widget>[
-                ElevatedButton.icon(
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.blue[400]), //Background Color
-                    elevation: MaterialStateProperty.all(0), //Defines Elevation
-                    // shadowColor:
-                    //     MaterialStateProperty.all(brown), //Defines shadowColor
-                  ),
-                  icon: Icon(Icons.person),
-                  label: Text('Login'),
-                  onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()));
-            },
-                ),
-              ],
+          actions: <Widget>[
+            ElevatedButton.icon(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                    Colors.blue[400]), //Background Color
+                elevation: MaterialStateProperty.all(0), //Defines Elevation
+                // shadowColor:
+                //     MaterialStateProperty.all(brown), //Defines shadowColor
+              ),
+              icon: Icon(Icons.person),
+              label: Text('Login'),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+            ),
+          ],
         ),
         body: Container(
-          child:SingleChildScrollView(
-           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
             child: Form(
               key: formkey,
               child: Column(
-                       mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                "Register",
-                 style: TextStyle(fontSize: 25),
-
-            ),
-             SizedBox(height: size.height * 0.05),
-            SvgPicture.asset(
-                "assets/icons/chat.svg",
-                height: size.height * 0.4,
-            ),
-                 SizedBox(height: size.height * 0.05),
+                    "Register",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  SizedBox(height: size.height * 0.05),
+                  SvgPicture.asset(
+                    "assets/icons/chat.svg",
+                    height: size.height * 0.4,
+                  ),
+                  SizedBox(height: size.height * 0.05),
                   Container(
                     child: TextFormField(
                       validator: (value) {
@@ -76,8 +75,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           return 'Username is invalid!';
                         }
 
-                          return null;
-
+                        return null;
                       },
                       decoration: InputDecoration(hintText: 'Username'),
                       onChanged: (value) {
@@ -112,7 +110,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                 "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}\$")
                             .hasMatch(value)) {
                           return 'Password should contain at least \none upper case \none lower case \none digit \none Special character \n8 characters in length.';
-                        }return null;
+                        }
+                        return null;
                       },
                       obscureText: true,
                       decoration: InputDecoration(hintText: 'Password'),
@@ -126,13 +125,14 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   SizedBox(height: size.height * 0.015),
                   InkWell(
                     onTap: () async {
-                      setState(() {
-                        if (formkey.currentState.validate()) {}
+                      setState(() async {
+                        if (formkey.currentState.validate()) {
+                          await HttpService.register(
+                              username, email, pass, context);
+                        }
                         ;
                       });
-                      await HttpService.register(username, email, pass, context);
                     },
-
                     child: Container(
                       margin:
                           EdgeInsets.symmetric(horizontal: 20, vertical: 10),
@@ -153,7 +153,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
             ),
           ),
-            )
-            );
+        ));
   }
 }

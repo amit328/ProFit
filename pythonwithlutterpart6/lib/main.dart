6935ae64af1dccb9dc398927e02df6d25ac39fa4
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-11-03 16:33:36 +0530

    added name and logo
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- pythonwithlutterpart6/lib/main.dart
+++ pythonwithlutterpart6/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 // import 'package:part5/views/welcome_screen.dart';
 import 'package:part5/views/login_screen.dart';
+import 'package:part5/views/dashboardScreen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      // home: LoginScreen(),
+      home: DashBoardPage(),
       builder: EasyLoading.init(),
     );
   }
